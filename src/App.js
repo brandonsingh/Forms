57413c855f9a5b661b0aa68ae69c6f451938478c
brandonsingh <brandonnsingh@gmail.com>
2020-06-29 15:10:31 -0400

    completed workshop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React, {Component} from 'react';
 
 class Forms extends Component {
-constructor() {
-  super();
-  
+constructor(props) {
+  super(props);
+  this.state = {
+    username: '',
+    password: '',
+  }
+};
 
+handleSubmit = (event) => {
+  //stop propagation
+  event.preventDefault();
+  alert(`an input was submitted: Username: ${this.state.username} Password: ${this.state.password}`)
+
+}
+
+handleChange = (event) => {
+  this.setState({[event.target.name]: event.target.value})
+  
+  console.log(this.state.username)
 }
 
   render() {
     return (
-      <div></div>
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="username">
+          Username: 
+          <input name ="username" onChange={this.handleChange} value={this.state.username} type="text" placeholder="Enter username"/>
+         </label>
+         <label htmlFor="password">
+          Password: 
+          <input name="password" onChange={this.handleChange} value={this.state.password} type="password" placeholder="Enter password"/>
+         </label>
+         <input type="submit"/>
+      </form>
     )
   };
 };
